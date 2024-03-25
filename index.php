03765fee86892e6bcde0b8800bad614fed5ed506
P. Pavithra <161850096+pp8189287@users.noreply.github.com>
2024-03-25 05:04:43 -0700

    index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mechanic Service</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+  </head>
+
+
+  <body style="background-image:url(images/bg2.jpeg);background-size:cover;">
+  <br>
+  <br><br>
+<br>
+  <br><br><br>
+  <br><br>
+  <div class="container">
+  <div class="jumbotron" style="background-color:transparent;" align="center">
+  <h1 style="color:#F9F3F3;">Welcome!</h1>
+  <p style="color:#FFE0E0;">We Provide you Vehicle Service Management system</p>
+  <p><a class="btn btn-primary btn-lg" href="login.php" role="button">User</a> <a class="btn btn-primary btn-lg" href="./admin/adminlogin.php" role="button">Admin</a></p>
+</div>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-1.11.3.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="js/bootstrap.js"></script>
+  </body>
+    
+</html>
