26a20609732847e3168bc5ba5bfe77983e876f59
P. Pavithra <161850096+pp8189287@users.noreply.github.com>
2024-03-25 05:10:02 -0700

    booking successfully.php

---
+++ booking successfully.php
@@ -0,0 +1,77 @@
+
+<?php if (!isset($_SESSION)) {
+  session_start();
+}
+require_once('Connections/breakcon.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$colname_Recordset1 = "-1";
+if (isset($_SESSION['MM_UserGroup'])) {
+  $colname_Recordset1 = $_SESSION['MM_UserGroup'];
+}
+mysql_select_db($database_breakcon, $breakcon);
+$query_Recordset1 = sprintf("SELECT * FROM register WHERE regno = %s", GetSQLValueString($colname_Recordset1, "int"));
+$Recordset1 = mysql_query($query_Recordset1, $breakcon) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+$totalRows_Recordset1 = mysql_num_rows($Recordset1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mechanic Service</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+        <script src="/js/jquery-1.11.3.min.js"></script>
+<script src="/js/bootstrap.js"></script>
+  </head>
+<body>
+<?php include 'header.php' ?>
+
+<h1 align="center">Your Service Booked Successfully</h1>
+
+
+
+
+</body>
+</html>
+
